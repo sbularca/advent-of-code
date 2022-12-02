2b0dd9cfe4763078f4b1f9f815855e04be1c8509
uzihead <sebastian.bularca@gmail.com>
2022-12-02 11:07:16 +0100

    Solved first puzzle

---
+++ adventofcode2022/adventofcode2022/AdventOfCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventOfCode2022 {
+    public interface IAdventOfCode {
+        void Start();
+    }
+
+    public static class AdventOfCode {
+        public static void Main(String[] args) {
+            Console.WriteLine("Executing Day 1");
+            Day1CalorieCounting day1 = new ();
+            day1.Start();
+        }
+    }
+}
---
+++ adventofcode2022/adventofcode2022/Day1CalorieCounting.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2022 {
+    public class Day1CalorieCounting : IAdventOfCode {
+        private const string url = "https://sebastianbularca.com/temp/data/day1Input.txt";
+
+        public void Start() {
+            Task task = Task.Run(async () => await GetInputDataFromUrlAsync(OnProcessResult));
+            while(!task.IsCompleted) {
+                Task.Yield();
+            }
+        }
+
+        private void OnProcessResult(string result) {
+            List<int> sums = new ();
+            var elvesBackpacks = result.Split(new [] {"\n\n"}, StringSplitOptions.None);
+            for(int i = 0; i < elvesBackpacks.Length; i++) {
+                string[] calories = elvesBackpacks[i].Split(new [] {"\n"}, StringSplitOptions.RemoveEmptyEntries);
+                int sum = 0;
+                for(int j = 0; j < calories.Length; j++) {
+                    sum += int.Parse(calories[j]);
+                }
+                sums.Add(sum);
+            }
+            sums.Sort();
+            Console.WriteLine($"The elf with the biggest muscles is carrying: {sums[^1]} calories. Such wonder!");
+        }
+
+        private async Task GetInputDataFromUrlAsync(Action<string> callback) {
+            HttpClient client = new ();
+            try {
+                string file = await client.GetStringAsync(url);
+                callback(file);
+            }
+            catch(HttpRequestException e) {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
