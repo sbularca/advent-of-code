052a7679a3fb0a031cc99a37620b9578bfb0ff9a
uzihead <sebastian.bularca@jovianindustries.com>
2022-12-06 11:56:35 +0100

    Added day 6 result

---
+++ adventofcode2022/Day6TuningTrouble.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2022 {
+    public class Day6TuningTrouble : IAdventOfCode {
+        private bool duplicate;
+        public string Url => "https://sebastianbularca.com/temp/data/day6Input.txt";
+        public DateTime Now { get; private set; }
+        public List<string> Results { get; } = new();
+
+        public void OnProcessData(string result) {
+            Now = DateTime.Now;
+            string dataStream = result;
+            var message1Length = 4;
+            var message2Length = 14;
+            GetMarker(dataStream, message1Length);
+            GetMarker(dataStream, message2Length);
+            
+            Console.WriteLine($"Day 2 OnProcessData method execution took {(DateTime.Now - Now).TotalMilliseconds}ms");
+        }
+
+        private void GetMarker(string dataStream, int messageLength) {
+            for (int i = 0; i < dataStream.Length; i++) {
+                if (i > dataStream.Length - 5) {
+                    break;
+                }
+
+                var a = 1;
+                string tempString = dataStream[i].ToString();
+                while (a < messageLength) {
+                    tempString += dataStream[i + a];
+                    a++;
+                }
+
+                if (tempString.Select(a => a).Distinct().Count() == tempString.Length) {
+                    Results.Add((i + messageLength).ToString());
+                    break;
+                }
+            }
+        }
+
+        public void PrintResults() {
+            Console.WriteLine($" \nShowing Day 6 results");
+            for(int i = 0; i < Results.Count; i++) {
+                Console.WriteLine($"Day 6 Part {i+1} Result - Index is {Results[i]}");
+            }
+        }
+    }
+}
