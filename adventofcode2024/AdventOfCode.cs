cc160e7a3c636f220e12c846b02d5c20b73a6751
Sebastian Bularca <sebastian.bularca@resolutiongames.com>
2024-12-02 14:30:00 +0100

    added advent of core 2024

---
+++ adventofcode2024/AdventOfCode.cs
@@ -0,0 +1,39 @@
+﻿public interface IAdventOfCode {
+    void ExecuteInstance(string dataSource);
+    void PrintResults();
+}
+
+public class AdventOfCode {
+    public static async Task Main(String[] args) {
+            var instances = new List<IAdventOfCode>();
+
+            var tasks = new List<Task>();
+            FindAllIAdvents(ref instances);
+
+            for(int i = 0; i < instances.Count; i++) {
+                IAdventOfCode aocInstance = instances[i];
+                var dataSource = GetInputData(aocInstance.GetType().Name);
+                tasks.Add(Task.Run(() => aocInstance.ExecuteInstance(dataSource)));
+            }
+
+            await Task.WhenAll(tasks);
+
+            for(int i = 0; i < instances.Count; i++) {
+                instances[i].PrintResults();
+            }
+        }
+
+        private static void FindAllIAdvents(ref List<IAdventOfCode> instances) {
+            IEnumerable<Type> adventsOfCode = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(t => t.GetInterfaces().Contains(typeof(IAdventOfCode)));
+            foreach (Type type in adventsOfCode) {
+                IAdventOfCode aoc = (IAdventOfCode)Activator.CreateInstance(type)!;
+                instances.Add(aoc);
+            }
+        }
+
+                private static string GetInputData(string fileName) {
+            return File.ReadAllText($"data/{fileName}.dat");
+        }
+}
---
+++ adventofcode2024/HistorianHysteria.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+ public class HistorianHysteria : IAdventOfCode {
+    public void ExecuteInstance(string dataSource) {
+        var lines = dataSource.Split('\n');
+
+    public void PrintResults() {
+        
+    }
+ }
