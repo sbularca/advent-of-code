6e708c5def76b0fc9fbad03bc9fa5938faf6afa5
Sebastian Bularca <sebastian.bularca@resolutiongames.com>
2024-12-09 15:43:54 +0100

    sda

--- adventofcode2024/CeresSearch.cs
+++ adventofcode2024/CeresSearch.cs
@@ -2,10 +2,27 @@
 
 public class CeresSearch : IAdventOfCode {
     private const string textToSearch = "XMAS";
+    private int counterPhase1 = 0;
+    private int counterPhase2;
     public bool ShouldRun { get; set; } = true;
     public void ExecuteInstance(string dataSource) {
+        //ExecutePhase1(dataSource);
+        ExecutePhase2(dataSource);
+    }
+
+    private void ExecutePhase2(string dataSource) {
+        Regex masRegex = new(@"M.S");
+        List<string> matrixLines = dataSource.Split("\n").ToList();
+
+        for(int i = 0; i < matrixLines.Count() - 2; i++) {
+            MatchCollection matches1 = masRegex.Matches(matrixLines[i]);
+            MatchCollection matches2 = masRegex.Matches(matrixLines[i + 2]);
+        }
+    }
+
+    private void ExecutePhase1(string dataSource) {
+        counterPhase1 = 0;
         Regex xmasRegex = new(@"XMAS");
-        int counter = 0;
         List<string> matrixLines = dataSource.Split("\n").ToList();
         List<string> invertedLines = matrixLines.Select(x => new string(x.Reverse().ToArray())).ToList();
 
@@ -23,13 +40,14 @@ public class CeresSearch : IAdventOfCode {
 
         foreach(string line in allLines) {
             MatchCollection matches = xmasRegex.Matches(line);
-            counter += matches.Count;
+            counterPhase1 += matches.Count;
         }
-
-        Console.WriteLine($"Found {counter} instances of {textToSearch}");
     }
 
-    public void PrintResults() { }
+    public void PrintResults() {
+        Console.WriteLine($"Found {counterPhase1} instances of {textToSearch}");
+        Console.WriteLine($"Found {counterPhase2} instances of M.S");
+    }
 
     private List<string> InvertMatrix(List<string> matrixLines) {
         List<string> matrixColumns = new();
@@ -46,7 +64,7 @@ public class CeresSearch : IAdventOfCode {
     }
 
     public static List<string> GetAllDiagonals(List<string> grid) {
-        List<string> diagonals = new List<string>();
+        List<string> diagonals = new();
 
         int rows = grid.Count;
         if(rows == 0) {
@@ -58,7 +76,7 @@ public class CeresSearch : IAdventOfCode {
         // Diagonals are defined by (row - col) = constant
         // Minimum (row - col) could be -(cols-1), maximum could be (rows-1)
         for(int diff = -(cols - 1); diff <= rows - 1; diff++) {
-            List<char> diagonalChars = new List<char>();
+            List<char> diagonalChars = new();
             for(int r = 0; r < rows; r++) {
                 int c = r - diff;
                 if(c >= 0 && c < cols) {
@@ -75,7 +93,7 @@ public class CeresSearch : IAdventOfCode {
         // Diagonals are defined by (row + col) = constant
         // Minimum (row + col) is 0, maximum (row + col) is (rows-1)+(cols-1)
         for(int sum = 0; sum <= rows - 1 + (cols - 1); sum++) {
-            List<char> diagonalChars = new List<char>();
+            List<char> diagonalChars = new();
             for(int r = 0; r < rows; r++) {
                 int c = sum - r;
                 if(c >= 0 && c < cols) {
@@ -89,7 +107,7 @@ public class CeresSearch : IAdventOfCode {
         }
 
         // Now generate the reversed versions of all the collected diagonals
-        List<string> reversedDiagonals = new List<string>();
+        List<string> reversedDiagonals = new();
         foreach(string d in diagonals) {
             char[] arr = d.ToCharArray();
             Array.Reverse(arr);
